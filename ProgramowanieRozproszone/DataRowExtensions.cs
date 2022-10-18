5f01a1468f14b67b4c51ad1b7a1325e88f16ec1b
Cyprian Gdowski <gdowskicyprian@gmail.com>
2022-10-18 13:04:34 +0200

    Added SqlProvider and datarowextensions to ease connection with database

---
+++ ProgramowanieRozproszone/DataRowExtensions.cs
@@ -0,0 +1,148 @@
+﻿using System.Data;
+using System.Text;
+
+namespace ProgramowanieRozproszone
+{
+    public static class DataRowExtensions
+    {
+        private const string ERROR_MESSAGE_DATAROW_NULL = "DataRow cannot be null!";
+
+        private static void ValidDataRow(DataRow row)
+        {
+            if (row is null)
+                throw new ArgumentNullException(ERROR_MESSAGE_DATAROW_NULL);
+        }
+
+        #region int and int?
+        public static int GetValueAsInt(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+            if (dataRow[columnName] == DBNull.Value)
+            {
+                dataRow[columnName] = 0;
+            }
+            return Convert.ToInt32(dataRow[columnName]);
+        }
+
+        public static int? GetValueAsNullableInt(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToInt32(dataRow[columnName]) : (int?)null;
+        }
+        #endregion
+
+        #region double and double?
+        public static double GetValueAsDouble(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return Convert.ToDouble(dataRow[columnName]);
+        }
+
+        public static double? GetValueAsNullableDouble(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToDouble(dataRow[columnName]) : (double?)null;
+        }
+        #endregion
+
+        #region decimal and decimal?
+        public static decimal GetValueAsDecimal(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+            if (dataRow[columnName] == DBNull.Value)
+            {
+                dataRow[columnName] = 0;
+            }
+            return Convert.ToDecimal(dataRow[columnName]);
+        }
+
+        public static decimal? GetValueAsNullableDecimal(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToDecimal(dataRow[columnName]) : (decimal?)null;
+        }
+        #endregion
+
+        #region bool
+        public static bool GetValueAsBool(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToBoolean(dataRow[columnName]) : true;
+        }
+        #endregion
+
+        #region string
+        public static string GetRowDataToString(this DataRow dataRow)
+        {
+            ValidDataRow(dataRow);
+
+            StringBuilder rowDataStringBuilder = new StringBuilder();
+
+            foreach (DataColumn column in dataRow.Table.Columns)
+                rowDataStringBuilder.AppendLine($"{column.ColumnName}='{dataRow[column.ColumnName]}'");
+
+            return rowDataStringBuilder.ToString();
+        }
+
+        public static string GetValueAsString(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow != null ? dataRow[columnName].ToString() : string.Empty;
+        }
+
+        public static string? GetValueAsNullableString(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? dataRow[columnName].ToString() : null;
+        }
+        #endregion
+
+        #region DateTime and DateTime?
+        public static DateTime GetValueAsDateTime(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return Convert.ToDateTime(dataRow[columnName].ToString());
+        }
+
+        public static DateTime? GetValueAsNullableDateTime(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToDateTime(dataRow[columnName]) : (DateTime?)null;
+        }
+        #endregion
+        #region Long
+        public static long GetValueAsLong(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return (long)dataRow[columnName];
+        }
+        #endregion
+
+        #region TimeSpan and TimeSpan?
+        public static TimeSpan GetValueAsTimeSpan(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return Convert.ToDateTime(dataRow[columnName].ToString()).TimeOfDay;
+        }
+
+        public static TimeSpan? GetValueAsNullableTimeSpan(this DataRow dataRow, string columnName)
+        {
+            ValidDataRow(dataRow);
+
+            return dataRow[columnName] != DBNull.Value ? Convert.ToDateTime(dataRow[columnName].ToString()).TimeOfDay : (TimeSpan?)null;
+        }
+        #endregion
+
+    }
+}
---
+++ ProgramowanieRozproszone/MsSqlProvider.cs
@@ -0,0 +1,60 @@
+﻿
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProgramowanieRozproszone
+{
+    public class MsSqlProvider
+    {
+        private string _connectionString;
+
+        public MsSqlProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = connection.CreateCommand())
+            using (var dataAdapter = new SqlDataAdapter(command))
+            {
+                try
+                {
+                    command.CommandTimeout = 1800;
+                    command.CommandText = query;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+        public void ExecuteQuery(out DataTable dataTable, string query)
+        {
+            dataTable = new DataTable();
+            using (var connection = new SqlConnection(_connectionString))
+            using (var command = connection.CreateCommand())
+            using (var dataAdapter = new SqlDataAdapter(command))
+            {
+                try
+                {
+                    command.CommandTimeout = 1800;
+                    command.CommandText = query;
+                    if(connection.State != ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    dataAdapter.Fill(dataTable);
+                }
+                finally 
+                { 
+                    connection.Close(); 
+                }
+            }
+        }
+    }
+}
