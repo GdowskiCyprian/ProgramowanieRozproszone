aa520372a16d6ec705f46bc90f0b8ee310f52c61
Cyprian Gdowski <gdowskicyprian@gmail.com>
2022-11-05 22:30:56 +0100

    added cors policy

--- ProgramowanieRozproszone/Program.cs
+++ ProgramowanieRozproszone/Program.cs
@@ -6,7 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("corsapp");
 app.UseAuthorization();
 
 app.MapControllers();
